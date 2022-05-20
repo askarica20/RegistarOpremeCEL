1aec99341e32ffc5f0605dadda657bfff7fd8b58
Ana Skarica <ana.skarica.as@gmail.com>
2022-05-20 18:37:26 +0200

    Add project files.

---
+++ RegistarOpremeCEL/Models/Oprema.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistarOpremeCEL.Models
+{
+    public class Oprema
+    {
+        public int Id { get; set; }
+        public string NazivOpreme { get; set; }
+        public string VrstaOpreme { get; set; }
+        public DateTime DatVrPrimke { get; set; }
+        public string NazivProjekta { get; set; }
+        public string IzvorFinanciranja { get; set; }
+        public string OpisOpreme { get; set; }
+        public string OsobaNabave { get; set; }
+        public string OsobaPrimke { get; set; }
+    }
+}
---
+++ RegistarOpremeCEL/frmUnosOpreme.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace RegistarOpremeCEL
+{
+    partial class FrmUnosOpreme
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNazivOpreme = new System.Windows.Forms.TextBox();
+            this.txtDatVrPrimke = new System.Windows.Forms.TextBox();
+            this.txtNazivProjekta = new System.Windows.Forms.TextBox();
+            this.txtOpisOpreme = new System.Windows.Forms.TextBox();
+            this.txtOsobaNabave = new System.Windows.Forms.TextBox();
+            this.txtOsobaPrimke = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblNaziv = new System.Windows.Forms.Label();
+            this.lblVrsta = new System.Windows.Forms.Label();
+            this.lblDatumVrijemePrimke = new System.Windows.Forms.Label();
+            this.lblNazivProjekta = new System.Windows.Forms.Label();
+            this.lblIzvorFinanciranja = new System.Windows.Forms.Label();
+            this.lblOpisOpreme = new System.Windows.Forms.Label();
+            this.lblOsobaNabave = new System.Windows.Forms.Label();
+            this.lblOsobaPrimke = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnUnesi = new System.Windows.Forms.Button();
+            this.cboVrstaOpreme = new System.Windows.Forms.ComboBox();
+            this.cboIzvorFinanciranja = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.BackColor = System.Drawing.Color.DarkGray;
+            this.txtId.Location = new System.Drawing.Point(336, 54);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(319, 20);
+            this.txtId.TabIndex = 0;
+            // 
+            // txtNazivOpreme
+            // 
+            this.txtNazivOpreme.BackColor = System.Drawing.Color.DarkGray;
+            this.txtNazivOpreme.Location = new System.Drawing.Point(336, 91);
+            this.txtNazivOpreme.Name = "txtNazivOpreme";
+            this.txtNazivOpreme.Size = new System.Drawing.Size(319, 20);
+            this.txtNazivOpreme.TabIndex = 1;
+            // 
+            // txtDatVrPrimke
+            // 
+            this.txtDatVrPrimke.BackColor = System.Drawing.Color.DarkGray;
+            this.txtDatVrPrimke.Location = new System.Drawing.Point(336, 163);
+            this.txtDatVrPrimke.Name = "txtDatVrPrimke";
+            this.txtDatVrPrimke.Size = new System.Drawing.Size(319, 20);
+            this.txtDatVrPrimke.TabIndex = 3;
+            // 
+            // txtNazivProjekta
+            // 
+            this.txtNazivProjekta.BackColor = System.Drawing.Color.DarkGray;
+            this.txtNazivProjekta.Location = new System.Drawing.Point(336, 203);
+            this.txtNazivProjekta.Name = "txtNazivProjekta";
+            this.txtNazivProjekta.Size = new System.Drawing.Size(319, 20);
+            this.txtNazivProjekta.TabIndex = 4;
+            // 
+            // txtOpisOpreme
+            // 
+            this.txtOpisOpreme.BackColor = System.Drawing.Color.DarkGray;
+            this.txtOpisOpreme.Location = new System.Drawing.Point(336, 284);
+            this.txtOpisOpreme.Name = "txtOpisOpreme";
+            this.txtOpisOpreme.Size = new System.Drawing.Size(319, 20);
+            this.txtOpisOpreme.TabIndex = 6;
+            // 
+            // txtOsobaNabave
+            // 
+            this.txtOsobaNabave.BackColor = System.Drawing.Color.DarkGray;
+            this.txtOsobaNabave.Location = new System.Drawing.Point(336, 323);
+            this.txtOsobaNabave.Name = "txtOsobaNabave";
+            this.txtOsobaNabave.Size = new System.Drawing.Size(319, 20);
+            this.txtOsobaNabave.TabIndex = 7;
+            // 
+            // txtOsobaPrimke
+            // 
+            this.txtOsobaPrimke.BackColor = System.Drawing.Color.DarkGray;
+            this.txtOsobaPrimke.Location = new System.Drawing.Point(336, 362);
+            this.txtOsobaPrimke.Name = "txtOsobaPrimke";
+            this.txtOsobaPrimke.Size = new System.Drawing.Size(319, 20);
+            this.txtOsobaPrimke.TabIndex = 8;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblId.ForeColor = System.Drawing.Color.Lime;
+            this.lblId.Location = new System.Drawing.Point(295, 50);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(35, 24);
+            this.lblId.TabIndex = 9;
+            this.lblId.Text = "ID:";
+            this.lblId.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNaziv
+            // 
+            this.lblNaziv.AutoSize = true;
+            this.lblNaziv.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblNaziv.ForeColor = System.Drawing.Color.Lime;
+            this.lblNaziv.Location = new System.Drawing.Point(253, 87);
+            this.lblNaziv.Name = "lblNaziv";
+            this.lblNaziv.Size = new System.Drawing.Size(77, 24);
+            this.lblNaziv.TabIndex = 10;
+            this.lblNaziv.Text = "NAZIV:";
+            // 
+            // lblVrsta
+            // 
+            this.lblVrsta.AutoSize = true;
+            this.lblVrsta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblVrsta.ForeColor = System.Drawing.Color.Lime;
+            this.lblVrsta.Location = new System.Drawing.Point(246, 122);
+            this.lblVrsta.Name = "lblVrsta";
+            this.lblVrsta.Size = new System.Drawing.Size(84, 24);
+            this.lblVrsta.TabIndex = 11;
+            this.lblVrsta.Text = "VRSTA:";
+            // 
+            // lblDatumVrijemePrimke
+            // 
+            this.lblDatumVrijemePrimke.AutoSize = true;
+            this.lblDatumVrijemePrimke.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblDatumVrijemePrimke.ForeColor = System.Drawing.Color.Lime;
+            this.lblDatumVrijemePrimke.Location = new System.Drawing.Point(55, 159);
+            this.lblDatumVrijemePrimke.Name = "lblDatumVrijemePrimke";
+            this.lblDatumVrijemePrimke.Size = new System.Drawing.Size(275, 24);
+            this.lblDatumVrijemePrimke.TabIndex = 12;
+            this.lblDatumVrijemePrimke.Text = "DATUM I VRIJEME PRIMKE:";
+            // 
+            // lblNazivProjekta
+            // 
+            this.lblNazivProjekta.AutoSize = true;
+            this.lblNazivProjekta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblNazivProjekta.ForeColor = System.Drawing.Color.Lime;
+            this.lblNazivProjekta.Location = new System.Drawing.Point(140, 198);
+            this.lblNazivProjekta.Name = "lblNazivProjekta";
+            this.lblNazivProjekta.Size = new System.Drawing.Size(190, 24);
+            this.lblNazivProjekta.TabIndex = 13;
+            this.lblNazivProjekta.Text = "NAZIV PROJEKTA:";
+            this.lblNazivProjekta.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lblIzvorFinanciranja
+            // 
+            this.lblIzvorFinanciranja.AutoSize = true;
+            this.lblIzvorFinanciranja.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblIzvorFinanciranja.ForeColor = System.Drawing.Color.Lime;
+            this.lblIzvorFinanciranja.Location = new System.Drawing.Point(98, 240);
+            this.lblIzvorFinanciranja.Name = "lblIzvorFinanciranja";
+            this.lblIzvorFinanciranja.Size = new System.Drawing.Size(232, 24);
+            this.lblIzvorFinanciranja.TabIndex = 14;
+            this.lblIzvorFinanciranja.Text = "IZVOR FINANCIRANJA:";
+            // 
+            // lblOpisOpreme
+            // 
+            this.lblOpisOpreme.AutoSize = true;
+            this.lblOpisOpreme.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblOpisOpreme.ForeColor = System.Drawing.Color.Lime;
+            this.lblOpisOpreme.Location = new System.Drawing.Point(173, 279);
+            this.lblOpisOpreme.Name = "lblOpisOpreme";
+            this.lblOpisOpreme.Size = new System.Drawing.Size(157, 24);
+            this.lblOpisOpreme.TabIndex = 15;
+            this.lblOpisOpreme.Text = "OPIS OPREME:";
+            // 
+            // lblOsobaNabave
+            // 
+            this.lblOsobaNabave.AutoSize = true;
+            this.lblOsobaNabave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblOsobaNabave.ForeColor = System.Drawing.Color.Lime;
+            this.lblOsobaNabave.Location = new System.Drawing.Point(12, 319);
+            this.lblOsobaNabave.Name = "lblOsobaNabave";
+            this.lblOsobaNabave.Size = new System.Drawing.Size(318, 24);
+            this.lblOsobaNabave.TabIndex = 16;
+            this.lblOsobaNabave.Text = "ODGOVORNA OSOBA NABAVE:";
+            // 
+            // lblOsobaPrimke
+            // 
+            this.lblOsobaPrimke.AutoSize = true;
+            this.lblOsobaPrimke.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblOsobaPrimke.ForeColor = System.Drawing.Color.Lime;
+            this.lblOsobaPrimke.Location = new System.Drawing.Point(20, 357);
+            this.lblOsobaPrimke.Name = "lblOsobaPrimke";
+            this.lblOsobaPrimke.Size = new System.Drawing.Size(310, 24);
+            this.lblOsobaPrimke.TabIndex = 17;
+            this.lblOsobaPrimke.Text = "ODGOVORNA OSOBA PRIMKE:";
+            this.lblOsobaPrimke.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Lime;
+            this.label10.Location = new System.Drawing.Point(12, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(60, 24);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "CEL |";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label11.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label11.Location = new System.Drawing.Point(67, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(162, 24);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "UNOS OPREME";
+            // 
+            // btnUnesi
+            // 
+            this.btnUnesi.BackColor = System.Drawing.Color.Lime;
+            this.btnUnesi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUnesi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnUnesi.Location = new System.Drawing.Point(533, 398);
+            this.btnUnesi.Name = "btnUnesi";
+            this.btnUnesi.Size = new System.Drawing.Size(122, 40);
+            this.btnUnesi.TabIndex = 20;
+            this.btnUnesi.Text = "UNESI";
+            this.btnUnesi.UseVisualStyleBackColor = false;
+            this.btnUnesi.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cboVrstaOpreme
+            // 
+            this.cboVrstaOpreme.BackColor = System.Drawing.Color.DarkGray;
+            this.cboVrstaOpreme.ForeColor = System.Drawing.Color.Black;
+            this.cboVrstaOpreme.FormattingEnabled = true;
+            this.cboVrstaOpreme.Items.AddRange(new object[] {
+            "Računalna oprema ",
+            "Komunikacijska oprema ",
+            "Multimedijska oprema",
+            "Licence",
+            "Pretplate",
+            "Ostalo"});
+            this.cboVrstaOpreme.Location = new System.Drawing.Point(336, 127);
+            this.cboVrstaOpreme.Name = "cboVrstaOpreme";
+            this.cboVrstaOpreme.Size = new System.Drawing.Size(319, 21);
+            this.cboVrstaOpreme.TabIndex = 21;
+            // 
+            // cboIzvorFinanciranja
+            // 
+            this.cboIzvorFinanciranja.BackColor = System.Drawing.Color.DarkGray;
+            this.cboIzvorFinanciranja.FormattingEnabled = true;
+            this.cboIzvorFinanciranja.Items.AddRange(new object[] {
+            "Temeljno financiranje znanosti \"5000,00 kn\" (potpora)",
+            "Znanstveno-nastavno usavršavanje \"5000,00 kn\"",
+            "Odluka - oprema - znan.nast., nast. i surad. zvanja \"4500,00 kn + PDV\"",
+            "EU projekt",
+            "Komercijalni projekt",
+            "Komercijalna djelatnost (edukacije, izrada aplikacija i dr.)",
+            "Sredstva proračuna (opći prihodi i primici)",
+            "Sredstva FOI (ostalo)"});
+            this.cboIzvorFinanciranja.Location = new System.Drawing.Point(336, 243);
+            this.cboIzvorFinanciranja.Name = "cboIzvorFinanciranja";
+            this.cboIzvorFinanciranja.Size = new System.Drawing.Size(319, 21);
+            this.cboIzvorFinanciranja.TabIndex = 22;
+            // 
+            // FrmUnosOpreme
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.ClientSize = new System.Drawing.Size(677, 450);
+            this.Controls.Add(this.cboIzvorFinanciranja);
+            this.Controls.Add(this.cboVrstaOpreme);
+            this.Controls.Add(this.btnUnesi);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblOsobaPrimke);
+            this.Controls.Add(this.lblOsobaNabave);
+            this.Controls.Add(this.lblOpisOpreme);
+            this.Controls.Add(this.lblIzvorFinanciranja);
+            this.Controls.Add(this.lblNazivProjekta);
+            this.Controls.Add(this.lblDatumVrijemePrimke);
+            this.Controls.Add(this.lblVrsta);
+            this.Controls.Add(this.lblNaziv);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.txtOsobaPrimke);
+            this.Controls.Add(this.txtOsobaNabave);
+            this.Controls.Add(this.txtOpisOpreme);
+            this.Controls.Add(this.txtNazivProjekta);
+            this.Controls.Add(this.txtDatVrPrimke);
+            this.Controls.Add(this.txtNazivOpreme);
+            this.Controls.Add(this.txtId);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmUnosOpreme";
+            this.Text = "Unos opreme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNazivOpreme;
+        private System.Windows.Forms.TextBox txtDatVrPrimke;
+        private System.Windows.Forms.TextBox txtNazivProjekta;
+        private System.Windows.Forms.TextBox txtOpisOpreme;
+        private System.Windows.Forms.TextBox txtOsobaNabave;
+        private System.Windows.Forms.TextBox txtOsobaPrimke;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblNaziv;
+        private System.Windows.Forms.Label lblVrsta;
+        private System.Windows.Forms.Label lblDatumVrijemePrimke;
+        private System.Windows.Forms.Label lblNazivProjekta;
+        private System.Windows.Forms.Label lblIzvorFinanciranja;
+        private System.Windows.Forms.Label lblOpisOpreme;
+        private System.Windows.Forms.Label lblOsobaNabave;
+        private System.Windows.Forms.Label lblOsobaPrimke;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnUnesi;
+        private System.Windows.Forms.ComboBox cboVrstaOpreme;
+        private System.Windows.Forms.ComboBox cboIzvorFinanciranja;
+    }
+}
+
